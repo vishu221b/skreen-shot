1a06db12c80b7d4e4a3dfd67d12ebb98a2ba5b31
vishu221b <vishal.dogra738@gmail.com>
2021-11-29 13:23:35 +0530

    Added openZeppelin upgradable support, Initialized project contract

---
+++ contracts/TokeenShot.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >0.7.0;
+
+import "../open-zep/contracts/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+
+contract TokeenSol is ERC721EnumerableUpgradeable{
+
+}
